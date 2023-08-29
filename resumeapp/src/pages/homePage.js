99f1394b39e4b7e5a8208708b7f24be1527bca26
CaptainFlopp3y <barrancofabian@gmail.com>
2023-08-29 12:41:33 -0600

    homepage

--- resumeapp/src/pages/homePage.js
+++ resumeapp/src/pages/homePage.js
@@ -1,6 +1,35 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
 const HomePage = () => {
     return (
-        <h1>HOMEPAGE TEST</h1>
+        <Container>
+            <Row>
+                <Col>
+                    <header>Full Stack Developer</header>
+                </Col>
+                <Col>
+                    <p>
+                        I create full stack web applications can
+                        be integraded with databases or third party SaaS systems
+                    </p>
+                    <p>
+                        Favorite front end frameworks to
+                        use are ReactJS, HandlebarsJS, Bootstrap,
+                        and Apollo.
+                    </p>
+                    Favorite backend technologies to
+                    use are ExpressJS, MongoDB, MySQL,
+                    NoSQL, and Apollo
+
+                    <p>
+
+                    </p>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 export default HomePage;
