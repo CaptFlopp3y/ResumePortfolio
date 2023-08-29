1ab504fe0e367fe10143dedefdb980922a153126
CaptainFlopp3y <barrancofabian@gmail.com>
2023-08-29 14:13:44 -0600

    project page

--- resumeapp/src/pages/projectPage.js
+++ resumeapp/src/pages/projectPage.js
@@ -1,7 +1,23 @@
+import Image from 'react-bootstrap/Image';
 
 const ProjectPage = () => {
     return (
-        <h1>project TEST</h1>
+        <main>
+            <header>Projects</header>
+            <div class="grid-container">
+                <div class="grid-item">
+                    <h1>Project name</h1>
+                    <p>Project details</p>
+                    <Image />
+                    <a>Site link</a>
+                </div>
+                <div class="grid-item">2</div>
+                <div class="grid-item">3</div>
+                <div class="grid-item">4</div>
+                <div class="grid-item">5</div>
+                <div class="grid-item">6</div>
+            </div>
+        </main>
     );
 };
 export default ProjectPage;
